3ec8dd319e7863042da60534c2925b95471b7fde
Ssong0515 <406605@naver.com>
2024-09-24 08:31:22 +0900

    초기 설정

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+      <div className={`container`}>
+          <h1>테스트 타이틀</h1>
+          <button className={`btn btn-primary`}>버튼</button>
+      </div>
+  )
+}
+
+export default App
