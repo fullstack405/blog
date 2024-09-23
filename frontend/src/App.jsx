eb2e0649102b870fdbfff17ee86a4aacb34c316d
Ssong0515 <406605@naver.com>
2024-09-24 08:57:58 +0900

    테스트3

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ function App() {
       <div className={`container`}>
           <h1>테스트 타이틀</h1>
           <button className={`btn btn-primary`}>버튼</button>
+
+          <button className={`btn btn-warning`}>버튼2</button>
+          <p>테스트3</p>
       </div>
   )
 }
