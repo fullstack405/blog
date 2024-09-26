d88c88572683a9102186e0f41c7d9856befa2202
Ssong0515 <406605@naver.com>
2024-09-26 09:44:53 +0900

    body 주소 수정

--- frontend/src/components/GridButton.jsx
+++ frontend/src/components/GridButton.jsx
@@ -5,16 +5,16 @@ import './GridButton.css'; // 스타일링 파일
 const GridButton = () => {
     // 버튼 및 이미지 정보 배열
     const buttons = [
-        { name: 'Android', image: '/blog/android.png', path: '/android' },
-        { name: 'Bootstrap', image: '/blog/bootstrap.png', path: '/bootstrap' },
-        { name: 'CSS', image: '/blog/css.png', path: '/css' },
-        { name: 'HTML', image: '/blog/html.png', path: '/html' },
-        { name: 'Java', image: '/blog/java.png', path: '/java' },
+        { name: 'Android', image: '/blog/android.png', path: '/body/android' },
+        { name: 'Bootstrap', image: '/blog/bootstrap.png', path: '/body/bootstrap' },
+        { name: 'CSS', image: '/blog/css.png', path: '/body/css' },
+        { name: 'HTML', image: '/blog/html.png', path: '/body/html' },
+        { name: 'Java', image: '/blog/java.png', path: '/body/java' },
         { name: 'JavaScript', image: '/blog/js.png', path: '/body/javascript' },
-        { name: 'JSP', image: '/blog/jsp.png', path: '/jsp' },
-        { name: 'React', image: '/blog/react.png', path: '/react' },
-        { name: 'Spring', image: '/blog/spring.png', path: '/spring' },
-        { name: 'Tomcat', image: '/blog/tomcat.webp', path: '/tomcat' },
+        { name: 'JSP', image: '/blog/jsp.png', path: '/body/jsp' },
+        { name: 'React', image: '/blog/react.png', path: '/body/react' },
+        { name: 'Spring', image: '/blog/spring.png', path: '/body/spring' },
+        { name: 'Tomcat', image: '/blog/tomcat.webp', path: '/body/tomcat' },
     ];
 
     return (
